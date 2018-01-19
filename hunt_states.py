a960f52aaa26f16b8240f896ed3c4c7c3748ecf9
TK Focht <tkfocht@alum.mit.edu>
2018-01-19 01:07:58 -0800

    More hunt types

--- hunt_states.py
+++ hunt_states.py
@@ -2,62 +2,103 @@ import json
 from scipy.stats import gamma, triang
 
 class Team:
-	def __init__(self, percentile_mode, percentile_lower, percentile_upper):
-		self.percentile_mode = percentile_mode
-		self.percentile_lower = percentile_lower
-		self.percentile_upper = percentile_upper
+    def __init__(self, percentile_mode, percentile_lower, percentile_upper):
+        self.percentile_mode = percentile_mode
+        self.percentile_lower = percentile_lower
+        self.percentile_upper = percentile_upper
 
-	def generate_regular_solve_percentile(self):
-		scale = self.percentile_upper - self.percentile_lower
-		shape = ((self.percentile_mode - self.percentile_lower) / float(self.percentile_upper - self.percentile_lower))
-		return triang.rvs(shape, scale=scale, loc=self.percentile_lower, size=1)[0]
+    def generate_regular_solve_percentile(self):
+        scale = self.percentile_upper - self.percentile_lower
+        shape = ((self.percentile_mode - self.percentile_lower) / float(self.percentile_upper - self.percentile_lower))
+        return triang.rvs(shape, scale=scale, loc=self.percentile_lower, size=1)[0]
 
 class SolvePattern:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def generate_solve_time(self):
-		pass
+    def generate_solve_time(self):
+        pass
 
 class IndependentGammaSolvePattern(SolvePattern):
-	def __init__(self, solve_pattern_dict):
-		self.avg_seed = solve_pattern_dict['avg_seed']
-		self.sd_seed = solve_pattern_dict['sd_seed']
-		self.scale = self.sd_seed * self.sd_seed / float(self.avg_seed)
-		self.shape = self.avg_seed / float(self.scale)
+    def __init__(self, solve_pattern_dict):
+        self.avg_seed = solve_pattern_dict['avg_seed']
+        self.sd_seed = solve_pattern_dict['sd_seed']
+        self.scale = self.sd_seed * self.sd_seed / float(self.avg_seed)
+        self.shape = self.avg_seed / float(self.scale)
 
-	def generate_solve_time(self, percentile):
-		return gamma.ppf(percentile, self.shape, scale=self.scale)
+    def generate_solve_time(self, percentile):
+        return gamma.ppf(percentile, self.shape, scale=self.scale)
 
 def generate_solve_pattern(solve_pattern_dict):
-	if solve_pattern_dict['rule'] == 'independent_gamma':
-		return IndependentGammaSolvePattern(solve_pattern_dict)
-	else:
-		raise ValueError
+    if solve_pattern_dict['rule'] == 'independent_gamma':
+        return IndependentGammaSolvePattern(solve_pattern_dict)
+    else:
+        raise ValueError
 
-def Unlock:
-	def __init__(self):
+
+
+class Unlock:
+    def __init__(self):
+        pass
+
+	def should_unlock(self, simulation):
+		return False
+
+class HuntStartUnlock(Unlock):
+	def __init__(self, unlock_dict):
 		pass
-	
+
+	def should_unlock(self, simulation):
+		return True
+
+class PuzzlesSolvedUnlock(Unlock):
+	def __init__(self, unlock_dict):
+		self.prereq_ids = unlock_dict['prereqs']
+		self.minimum = unlock_dict['minimum'] if 'minimum' in unlock_dict else len(self.prereq_ids)
+
+	def should_unlock(self, simulation):
+		return len([s for s in [simulation.puzzle_states[k] for k in self.prereq_ids] if s == 'SOLVED']) >= self.minimum
+
+class ScoreMinimumUnlock(Unlock):
+	def __init__(self, unlock_dict):
+		self.score = unlock_dict['score']
+
+	def should_unlock(self, simulation):
+		return simulation.team_state['score'] >= self.score
+
+def generate_unlock(unlock_dict):
+    if unlock_dict['rule'] == 'hunt_start':
+        return HuntStartUnlock(unlock_dict)
+    elif unlock_dict['rule'] == 'puzzles_solved':
+    	return PuzzlesSolvedUnlock(unlock_dict)
+    elif unlock_dict['rule'] == 'score_minimum':
+    	return ScoreMinimumUnlock(unlock_dict)
+    else:
+        raise ValueError
+
 
 class PuzzleDefinition:
-	def __init__(self, puzzle_definition_dict):
-		self.id = puzzle_definition_dict['id']
-		self.solve_pattern = generate_solve_pattern(puzzle_definition_dict['solve_pattern'])
+    def __init__(self, puzzle_definition_dict):
+        self.id = puzzle_definition_dict['id']
+        self.solve_pattern = generate_solve_pattern(puzzle_definition_dict['solve_pattern'])
+        self.unlock = generate_unlock(puzzle_definition_dict['unlock'])
+        self.reward = puzzle_definition_dict['reward'] if 'reward' in puzzle_definition_dict else None
 
-	def __repr__(self):
-		return 'PuzzleDefinition: {{ id: {0}, p50: {1} }}'.format(self.id, self.solve_pattern.generate_solve_time(0.5))
+    def __repr__(self):
+        return 'PuzzleDefinition: {{ id: {0}, p50: {1} }}'.format(self.id, self.solve_pattern.generate_solve_time(0.5))
 
 class HuntDefinition:
-	def __init__(self, filename):
-		hunt_definition_data = json.load(open(filename))
-		if 'puzzles' in hunt_definition_data:
-			self.puzzle_definitions = [PuzzleDefinition(p_dict) for p_dict in hunt_definition_data['puzzles']]
+    def __init__(self, filename):
+        hunt_definition_data = json.load(open(filename))
+        if 'puzzles' in hunt_definition_data:
+            self.puzzle_definitions = [PuzzleDefinition(p_dict) for p_dict in hunt_definition_data['puzzles']]
+        self.end_state_unlock = generate_unlock(hunt_definition_data['end_state'])
+        self.initial_team_state = hunt_definition_data['initial_team_state'] if 'initial_team_state' in hunt_definition_data else None
 
 class TeamHuntProjection:
-	def __init__(self, hunt_definition, team):
-		def project(puzzle_definition):
-			team_pctile = team.generate_regular_solve_percentile()
-			solve_time = puzzle_definition.solve_pattern.generate_solve_time(team_pctile)
-			return int(solve_time)
-		self.puzzle_projections = { p_def.id: project(p_def) for p_def in hunt_definition.puzzle_definitions }
+    def __init__(self, hunt_definition, team):
+        def project(puzzle_definition):
+            team_pctile = team.generate_regular_solve_percentile()
+            solve_time = puzzle_definition.solve_pattern.generate_solve_time(team_pctile)
+            return int(solve_time)
+        self.puzzle_projections = { p_def.id: project(p_def) for p_def in hunt_definition.puzzle_definitions }
--- simulate.py
+++ simulate.py
@@ -1,16 +1,66 @@
 from hunt_states import *
 
 class Simulation:
-	def __init__(self, hunt_definition, team):
-		self.team_hunt_projection = TeamHuntProjection(hunt_definition, team)
-		self.current_time = 0
+    def __init__(self, hunt_definition, team):
+        self.hunt_definition = hunt_definition
+        self.puzzle_definitions = { p.id: p for p in self.hunt_definition.puzzle_definitions }
+        self.team_hunt_projection = TeamHuntProjection(hunt_definition, team)
+        self.current_time = 0
+        self.puzzle_states = { p.id: 'LOCKED' for p in hunt_definition.puzzle_definitions }
+        self.puzzle_history = {}
+        self.team_state = hunt_definition.initial_team_state if hunt_definition.initial_team_state else {}
+        self.complete = False
+
+    def update_unlocks(self):
+        updates_made = False
+        for pid in [pk for pk in self.puzzle_states.keys() if self.puzzle_states[pk] == 'LOCKED']:
+            p = self.puzzle_definitions[pid]
+            if p.unlock.should_unlock(self):
+                self.puzzle_states[p.id] = 'UNLOCKED'
+                if p.id not in self.puzzle_history:
+                    self.puzzle_history[p.id] = {}
+                self.puzzle_history[p.id]['UNLOCKED'] = self.current_time
+                updates_made = True
+        if updates_made:
+            self.update_unlocks()
+
+    def update_solves(self):
+        for pid in [pk for pk in self.puzzle_states.keys() if self.puzzle_states[pk] == 'UNLOCKED']:
+            p = self.puzzle_definitions[pid]
+            if self.team_hunt_projection.puzzle_projections[pid] + self.puzzle_history[pid]['UNLOCKED'] <= self.current_time:
+                self.puzzle_states[p.id] = 'SOLVED'
+                self.puzzle_history[p.id]['SOLVED'] = self.current_time
+                if p.reward:
+                    for k in p.reward.keys():
+                        self.team_state[k] += p.reward[k]
+
+
+    def advance(self):
+        self.current_time += 1
+        self.update_solves()
+        self.update_unlocks()
+        if (self.hunt_definition.end_state_unlock.should_unlock(self)):
+            self.complete = True
+
+
+    def __repr__(self):
+        return '{{ current_time: {0}, puzzle_states: {{ {1} }}, team_state: {2} }}' \
+            .format(self.current_time, ', '.join([k + ': ' + v for k,v in self.puzzle_states.iteritems()]), self.team_state)
+
 
 if __name__ == '__main__':
-	hunt_definition = HuntDefinition('linear_hunt.json')
-	print(hunt_definition)
-	print(hunt_definition.puzzle_definitions)
+    hunt_definition = HuntDefinition('loose_prereq_hunt.json')
+    print(hunt_definition)
+    print(hunt_definition.puzzle_definitions)
 
-	team = Team(0.25, 0.1, 0.75)
-	
-	team_hunt_projection = TeamHuntProjection(hunt_definition, team)
-	print(team_hunt_projection.puzzle_projections)
+    team = Team(0.5, 0.05, 0.95)
+    
+    team_hunt_projection = TeamHuntProjection(hunt_definition, team)
+    print(team_hunt_projection.puzzle_projections)
+
+    sim = Simulation(hunt_definition, team)
+    print(sim)
+
+    while not sim.complete:
+        sim.advance()
+        print(sim)
